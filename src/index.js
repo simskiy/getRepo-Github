769b1ee3d0000adff3c82809ee34b745c778f038
simskiy <simskiy@gmail.com>
2022-08-16 14:41:37 +0300

    Инициализация проекта

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles/main.scss'
+import '@js/sum.js'
+
+
+// eslint-disable-next-line quotes
+console.log("it's working!!!")
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+const webpack = require('webpack')
+
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+// eslint-disable-next-line max-len
+const filename = (ext, hash) => isDev ? `[name].${ext}` : `[name].[${hash}:8].${ext}`
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: filename('js', 'chunkhash'),
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@js': path.resolve(__dirname, 'src/js')
+    }
+  },
+
+  mode: isProd ? 'production' : 'development',
+  devtool: isDev ? 'source-map' : false,
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      title: 'Get Repo GitHub',
+      favicon: './favicon.ico'
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css', 'contenthash')
+    }),
+    new ESLintPlugin(),
+  ],
+
+  devServer: {
+    compress: true,
+    port: 9000,
+    hot: isDev,
+    open: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ],
+  },
+};
